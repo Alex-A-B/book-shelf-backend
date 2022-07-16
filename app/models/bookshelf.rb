454f451b4f7b1c618e5c8e32fd802c6b07d48275
Alex-A-B <alexjamesbelton@gmail.com>
2022-07-16 14:10:27 +0100

    rspec testing framework added

--- app/models/bookshelf.rb
+++ app/models/bookshelf.rb
@@ -1,5 +1,5 @@
 class Bookshelf < ApplicationRecord
-    
+    include Rails.application.routes.url_helpers
     belongs_to :user
     belongs_to :book
 
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ApplicationRecord
 
-    validates :rating, numericality: { in: 1..5 }
+    validates :rating, numericality: { in: 1..5 }, presence: true
+    validates :book_id, presence: true
 
     belongs_to :user
     belongs_to :book
